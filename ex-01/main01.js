10b43c992af621f8b3c76a74dc6646a7c7c8ad67
drissnafii <drissnafi3@gmail.com>
2024-12-05 16:27:59 +0100

    ex 02 & ex 03

--- ex-01/main01.js
+++ ex-01/main01.js
@@ -1,4 +1,14 @@
-
+// EXERCICES 
+// 1. Changer la couleur du corps de la page lors d'un clic.
+// 2. Afficher une alerte avec un message personnalisé lors d'un clic sur un bouton.
+// 3. Afficher la position de la souris sur la page en temps réel.
+// 4. Créer une boîte modale qui s'affiche et se ferme avec un bouton.
+// 5. Compter le nombre de fois qu'un bouton est cliqué.
+// 6. Alterner la visibilité d'un élément lors d'un clic (montrer/cacher).
+// 7. Modifier le texte d'un élément au survol de la souris.
+// 8. Valider un champ de saisie pour qu'il contienne au moins 5 caractères.
+// 9. Ajouter une classe CSS à un élément lors d'un clic.
+// 10. Créer une minuterie qui affiche un compte à rebours sur la page.
 
 
 // exercice 01 ==> function 01
@@ -14,7 +24,17 @@ function changeColor() {
 }
 
 // exercice 02 ==> function 02
+function alertFn() {
+    alert('Button clicked')
+}
 
+// 3. Afficher la position de la souris sur la page en temps réel.
+document.addEventListener('mousemove', function (event) {
+    const x = event.clientX
+    const y = event.clientY
 
+    document.getElementById('X').textContent = `M(${x}, ${y})`
+})
 
+// 4. Créer une boîte modale qui s'affiche et se ferme avec un bouton.
 
